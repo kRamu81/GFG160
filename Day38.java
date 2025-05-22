c8cc608ef681554c7bb198823020821d2ecc6d4e
K Ramu <kanamramu18@gmail.com>
2025-05-22 19:46:14 +0530

    Create Day38.java

---
+++ Day38.java
@@ -0,0 +1,19 @@
+class Solution {
+public static boolean matSearch(int mat[][], int x) {
+    // your code here
+    int n=mat.length,m=mat[0].length;
+    int i=0,j=m-1;
+    while(i<n&&j>=0){
+        if(x>mat[i][j]){
+            i++;
+        }
+        else if(x<mat[i][j]){
+            j--;
+        }
+        else{
+            return true;
+        }
+    }
+    return false;
+}
+}
