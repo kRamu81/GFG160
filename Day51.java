615ddecc95bf093dc8d4c0f68aa7b01b01abf81e
K Ramu <kanamramu18@gmail.com>
2025-05-22 19:52:46 +0530

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,38 @@
+class Solution {
+public int countTriplets(int[] arr, int target) {
+    // Code Here
+    Arrays.sort(arr);
+    int n=arr.length;
+    int res=0;
+    for(int i=0;i<n-2;i++){
+        int left=i+1;
+        int right=n-1;
+        while(left < right){
+            int sum=arr[i]+arr[left]+arr[right];
+            if(sum == target){
+                int ele1=arr[left],ele2=arr[right];
+                int cnt1=0,cnt2=0;
+                while(left <= right && arr[left] == ele1){
+                    cnt1++;
+                    left++;
+                }
+                while(left <= right && arr[right] == ele2){
+                    cnt2++;
+                    right--;
+                }
+                if(ele1 == ele2){
+                    res+=(cnt1*(cnt1-1))/2;
+                }
+                else{
+                    res+=cnt1*cnt2;
+                }
+            }else if(sum < target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+    }
+    return res;
+}
+}
