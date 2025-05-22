3cacdb54e33ad6ab88f02f22173fdd80fd0d8c1d
K Ramu <kanamramu18@gmail.com>
2025-05-22 19:53:40 +0530

    Create Day53.java

---
+++ Day53.java
@@ -0,0 +1,24 @@
+class Solution {
+public List<Integer> sumClosest(int[] arr, int target) {
+    // code here
+    int n=arr.length;
+    Arrays.sort(arr);
+    List<Integer> res=new ArrayList<>();
+    int minDiff=Integer.MAX_VALUE;
+    int left=0,right=n-1;
+    while(left<right){
+        int currSum=arr[left]+arr[right];
+        if(Math.abs(target-currSum)<minDiff){
+            minDiff=Math.abs(target-currSum);
+            res=Arrays.asList(arr[left],arr[right]);
+        }
+        if(currSum < target)
+            left++;
+        else if (currSum > target)
+            right--;
+        else
+            return res;
+    }
+    return res;
+}
+}
