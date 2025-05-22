4f3989c256fc14bf5e754452598c8d3b504920b3
K Ramu <kanamramu18@gmail.com>
2025-05-22 19:48:48 +0530

    Create Day42.java

---
+++ Day42.java
@@ -0,0 +1,15 @@
+class Solution {
+boolean twoSum(int arr[], int target) {
+    HashSet<Integer> set=new HashSet<>();
+    
+    for(int i=0;i<arr.length;i++){
+        int complement=target-arr[i];
+        
+        if(set.contains(complement)){
+            return true;
+        }
+        set.add(arr[i]);
+    }
+    return false;
+}
+}
