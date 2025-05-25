1f18f817b5aeaac83e01fb8652364e6a228eadc4
K Ramu <kanamramu18@gmail.com>
2025-05-25 17:22:53 +0530

    Create Day63.java

---
+++ Day63.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public int maxLen(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0, maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += (arr[i] == 0) ? -1 : 1;
+
+            if (sum == 0)
+                maxLen = i + 1;
+
+            if (map.containsKey(sum))
+                maxLen = Math.max(maxLen, i - map.get(sum));
+            else
+                map.put(sum, i);
+        }
+        return maxLen;
+    }
+}
